d9fec0fbdc78469f41bfc75d2ef9684ab8a09e85
Akmaral <axma.mail@gmail.com>
2022-10-13 22:42:12 +0600

    Задача 24

---
+++ Ex001/Program.cs
@@ -0,0 +1,14 @@
+﻿// ЗАдача 24. Напишите прогармму которая принимает на вход число (А)
+// и выдаёт сумму чисел от 1 до А
+Console.Clear();
+Console.Write("введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 1, result = 0;
+while (i <= n)
+{
+    result += i;
+    i++;
+}
+Console.WriteLine(result);
+// Можно записать код иначе:
+// Console.WriteLine((1 + n) / 2 * n);
