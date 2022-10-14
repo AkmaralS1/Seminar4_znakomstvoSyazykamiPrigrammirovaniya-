daaa5b685c9ee57c7fb4ee0a7dbf792d7b50a718
Akmaral <axma.mail@gmail.com>
2022-10-14 19:42:13 +0600

    Задача 26

---
+++ Ex002/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 26. Напишите программу которая принимает на вход число
+// и выдаёт колличество цифр в числе
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int resault = 0;
+while (n > 0)
+{
+    n = n / 10;
+    resault++;
+}
+Console.WriteLine(resault);
